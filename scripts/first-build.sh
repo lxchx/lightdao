28d680dd9d76f27d2102c40fc6d14ebc77b8a848
lxchx <lc.9816@163.com>
2025-01-23 08:13:15 +0000

    fix: release ci 双图标问题

--- scripts/first-build.sh
+++ scripts/first-build.sh
@@ -6,16 +6,24 @@ SCRIPT_DIR=$(dirname "$0")
 # 构建AndroidManifest.xml的相对路径
 MANIFEST_PATH="$SCRIPT_DIR/../android/app/src/main/AndroidManifest.xml"
 
-# 取消注释AndroidManifest.xml中的特定代码块
-sed -i '/<!-- START: build-first-time-comment -->/,/<!-- END: build-first-time-comment -->/s/<!--//' "$MANIFEST_PATH"
-sed -i '/<!-- START: build-first-time-comment -->/,/<!-- END: build-first-time-comment -->/s/-->//' "$MANIFEST_PATH"
+# 取消注释以通过flutter编译条件
+sed -i '/<!-- START: build-first-time-comment -->/,/<!-- END: build-first-time-comment -->/ {
+    # 匹配起始行（<!--<intent-filter>...）
+    s/^\([[:space:]]*\)<!--\(.*intent-filter.*\)/\1\2/
+    # 匹配结束行（</intent-filter>-->）
+    s/^\([[:space:]]*\)\(.*intent-filter.*\)-->/\1\2/
+}' "$MANIFEST_PATH"
 
-# 第一次构建APK
+# 构建APK，此时将有两个启动图标
 flutter build apk --release
 
-# 再次注释AndroidManifest.xml中的特定代码块
-sed -i '/<!-- START: build-first-time-comment -->/,/<!-- END: build-first-time-comment -->/s/^/<!-- /' "$MANIFEST_PATH"
-sed -i '/<!-- START: build-first-time-comment -->/,/<!-- END: build-first-time-comment -->/s/$/ -->/' "$MANIFEST_PATH"
+# 重新注释
+sed -i '/<!-- START: build-first-time-comment -->/,/<!-- END: build-first-time-comment -->/ {
+    # 匹配起始行（<intent-filter>...）
+    s/^\([[:space:]]*\)\(<intent-filter>\)/\1<!--\2/
+    # 匹配结束行（</intent-filter>）
+    s/^\([[:space:]]*\)\(<\/intent-filter>\)/\1\2-->/
+}' "$MANIFEST_PATH"
 
-# 第二次构建APK
+# 正确构建APK
 flutter build apk --release
