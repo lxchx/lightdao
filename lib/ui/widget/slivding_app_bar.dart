23e7dfd1e498029a3ef3d951fa6f554204d35349
lxchx <lc.9816@163.com>
2025-05-10 22:51:26 +0800

    feat: 更丝滑的SlidingAppBar动画

--- lib/ui/widget/slivding_app_bar.dart
+++ lib/ui/widget/slivding_app_bar.dart
@@ -14,32 +14,78 @@ class SlidingAppBar extends StatefulWidget implements PreferredSizeWidget {
   final bool visible;
 
   @override
-  Size get preferredSize => visible ? child.preferredSize : Size.zero;
+  Size get preferredSize => child.preferredSize;
 
   @override
   State<SlidingAppBar> createState() => _SlidingAppBarState();
 }
 
 class _SlidingAppBarState extends State<SlidingAppBar> with SingleTickerProviderStateMixin {
-  late AnimationController controller;
+  late AnimationController _controller;
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(
+    _controller = AnimationController(
       vsync: this,
-      duration: widget.duration, 
+      duration: widget.duration,
     );
+
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: widget.curve),
+    );
+
+    if (widget.visible) {
+      _controller.value = 1.0; // 完全可见
+    } else {
+      _controller.value = 0.0; // 完全隐藏
+    }
+  }
+
+  @override
+  void didUpdateWidget(SlidingAppBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.visible != oldWidget.visible) {
+      if (widget.visible) {
+        _controller.forward(); // 可见状态
+      } else {
+        _controller.reverse(); // 隐藏状态
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    widget.visible ? controller.reverse() : controller.forward();
-    return SlideTransition(
-      position: Tween<Offset>(begin: Offset.zero, end: Offset(0, -1)).animate(
-        CurvedAnimation(parent: controller, curve: widget.curve),
-      ),
-      child: widget.child,
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return PreferredSize(
+          // 使用动画的值来计算当前的 preferredSize
+          preferredSize: Size(
+            widget.child.preferredSize.width,
+            widget.child.preferredSize.height * _animation.value,
+          ),
+          child: ClipRect( // 使用 ClipRect 来裁剪内容，使其平滑消失/出现
+            child: Align(
+              alignment: Alignment.topCenter,
+              heightFactor: _animation.value, // 高度因子也使用动画值
+              child: SlideTransition(
+                position: Tween<Offset>(begin: Offset(0, -1), end: Offset.zero).animate(
+                  CurvedAnimation(parent: _controller, curve: widget.curve),
+                ),
+                child: widget.child,
+              ),
+            ),
+          ),
+        );
+      },
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
